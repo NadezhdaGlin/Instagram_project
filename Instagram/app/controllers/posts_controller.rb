b61b3ac1c5b3ccd20f8c1ce2ff2e642f1a3935c2
Nadezhda Glin <glinovanad@gmail.com>
2022-05-01 20:59:02 +0300

    posts controller

---
+++ Instagram/app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:description, :image)
+    end
+
+end
