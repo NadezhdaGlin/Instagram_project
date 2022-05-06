be99ea4f5185a25dd633ed1718844e6bd9d385d7
Nadezhda Glin <glinovanad@gmail.com>
2022-05-06 15:20:20 +0300

    model post

--- Instagram/app/models/post.rb
+++ Instagram/app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
 	mount_uploader :image, ImageUploader
 	validates :description, :image, presence: true
 	has_many :comments, dependent: :destroy
-
+	has_many :likes, dependent: :destroy
 end
