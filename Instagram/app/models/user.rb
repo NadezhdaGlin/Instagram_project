54b1455180ed83a649551c37df25ccec0f99f4a6
Nadezhda Glin <glinovanad@gmail.com>
2022-05-08 21:39:14 +0300

    model user

--- Instagram/app/models/user.rb
+++ Instagram/app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  mount_uploader :image, ImageUploader
+
 end
