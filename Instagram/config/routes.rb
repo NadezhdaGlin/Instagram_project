b19afce44552450b5862f30ef34efca978f5187b
Nadezhda Glin <glinovanad@gmail.com>
2022-05-13 11:27:27 +0300

    add routes to followers and following

--- Instagram/config/routes.rb
+++ Instagram/config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
 
-  resources :users
   resources :posts do
     resources :comments
   end
   resources :likes
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :relationships, only: [:create, :destroy]
 
 end
