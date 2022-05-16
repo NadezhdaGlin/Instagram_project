1fa3287df78a92cd5edd2745918281e3dffbac1b
Nadezhda Glin <glinovanad@gmail.com>
2022-05-16 11:24:51 +0300

    added index and show

--- Instagram/app/controllers/users_controller.rb
+++ Instagram/app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
 
+	def index
+		@users = User.where.not(id: current_user)
+	end
+
 	def edit; end
+
+	def show
+		@user = User.find(params[:id])
+	end
 
 	def update
 		if current_user.update(user_params)
