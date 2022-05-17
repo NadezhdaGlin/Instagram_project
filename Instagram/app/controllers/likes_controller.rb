87831a26b39b77a009e1c193dea1b80fcff8f080
Nadezhda Glin <glinovanad@gmail.com>
2022-05-17 10:27:06 +0300

    add create and destroy like

--- Instagram/app/controllers/likes_controller.rb
+++ Instagram/app/controllers/likes_controller.rb
@@ -1,13 +1,15 @@
 class LikesController < ApplicationController
+
   def create
-    @post = Post.find(params[:post_id])
+    @post = Post.find(params[:format])
     @like = @post.likes.create(user: current_user, post: @post)
+    redirect_to @post
   end
 
-  def destroy 
-    @post = Post.find(params[:post_id])
-    @like = @post.likes.find(params[:id])
-    @like.destroy
+  def destroy
+    @post = Post.find(params[:id])
+    @like = Like.where(user: current_user, post: @post)
+    @like.destroy_by(id: @like.ids)
+    redirect_to @post
   end
-
 end
