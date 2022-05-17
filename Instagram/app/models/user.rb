88f403e1e0ac17f3a66dd347d4264d234141f1a3
Nadezhda Glin <glinovanad@gmail.com>
2022-05-17 10:34:06 +0300

    added has_many likes

--- Instagram/app/models/user.rb
+++ Instagram/app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   mount_uploader :image, ImageUploader
 
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
