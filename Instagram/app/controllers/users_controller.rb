30cb6edb0a190f740f0441746284c8301a63225a
Nadezhda Glin <glinovanad@gmail.com>
2022-05-18 23:23:09 +0300

    extra blank lines removed

--- Instagram/app/controllers/users_controller.rb
+++ Instagram/app/controllers/users_controller.rb
@@ -1,27 +1,27 @@
 class UsersController < ApplicationController
 
-  def index
-		@users = User.where.not(id: current_user)
-	end
+def index
+	@users = User.where.not(id: current_user)
+end
 
-	def edit; end
+def edit; end
 
-	def show
-		@user = User.find(params[:id])
-	end
+def show
+	@user = User.find(params[:id])
+end
 
-	def update
-		if current_user.update(user_params)
-			redirect_to current_user
-		else
-			render :edit, status: :unprocessable_entity
-		end
+def update
+	if current_user.update(user_params)
+		redirect_to current_user
+	else
+		render :edit, status: :unprocessable_entity
 	end
+end
 	
-	private
+private
 
-		def user_params
-			params.require(:user).permit(:name, :about, :image)
-		end
+	def user_params
+		params.require(:user).permit(:name, :about, :image)
+	end
 end
 
