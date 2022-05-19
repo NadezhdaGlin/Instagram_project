97050f072318d258ecea252cb5b1099fa4da7ed9
Nadezhda Glin <glinovanad@gmail.com>
2022-05-19 08:50:10 +0300

    added STDOUT.sync

--- Instagram/config/environments/production.rb
+++ Instagram/config/environments/production.rb
@@ -86,7 +86,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger           = ActiveSupport::Logger.new($stdout)
+    STDOUT.sync = true
+    logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
