25dc0f2b8bd5a9922fad39c4c2188aeff2dacb53
Nadezhda Glin <glinovanad@gmail.com>
2022-06-13 20:47:28 +0300

    comments controller

--- Instagram/app/controllers/comments_controller.rb
+++ Instagram/app/controllers/comments_controller.rb
@@ -17,6 +17,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:commenter, :body).merge(user: current_user)
+    params.require(:comment).permit(:body).merge(user: current_user)
   end
 end
